a3d7aff628329342e3ba3dd1132057832363b681
scokolDev <149943187+scokolDev@users.noreply.github.com>
2023-11-05 06:21:09 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,98 @@
+//init all media containers
+const videoWindow = document.getElementById("videoWindow");
+const friendly = document.getElementById("friendly");
+const crosshairView = document.getElementById("crosshairView");
+const enemy = document.getElementById("enemy");
+const crosshairDotView = document.getElementById("crosshairDotView");
+const infoBox = document.getElementById("source");
+
+//bool for is full screen
+let isFull = false;
+
+//clear content of all media containers
+function clearMedia(){
+    videoWindow.setAttribute('src', "");
+    friendly.setAttribute('src', "");
+    crosshairView.setAttribute('src', "");
+    crosshairDotView.setAttribute('src', "");
+    enemy.setAttribute('src', "");
+    friendly.style.boxShadow = "";
+    enemy.style.boxShadow = "";
+    infoBox.innerHTML = "";
+}
+
+//fill the index list with all map smoke entries from given array
+function populateIndex(list){
+    for (let i = 0; i < list.length; i++) {
+        let str = toString(i);
+        let option = document.createElement("button");
+        option.innerHTML += '<img class="indexIcon" src="icons/' + (list[i].side == "t" ? 't.png' : 'ct.png') + '">'; 
+            
+        option.innerHTML += list[i].start + " : " + list[i].land;
+        if(list[i].site == 'a' || list[i].site == 'b'){ 
+            option.innerHTML += '<img class="indexIcon" src="icons/' + list[i].site +  '.png' + '">';
+        }else{
+            option.innerHTML += '<img class="indexIcon" src="icons/m.png">';
+        }
+        option.setAttribute("class", "indexItem");
+        option.style.background = list[i].side == "t" ? "rgba(255, 165, 0, 0.329)" : "rgba(0, 0, 255, 0.329)";
+        option.addEventListener("click", function() {
+            clearMedia();
+            if(list[i].side == "t"){
+                friendly.style.boxShadow = "rgba(100, 50, 0, 0.56) 0px 22px 70px 4px";
+                enemy.style.boxShadow = "rgba(0, 0, 50, 0.56) 0px 22px 70px 4px";
+            }else{
+                friendly.style.boxShadow = "rgba(0, 0, 50, 0.56) 0px 22px 70px 4px";
+                enemy.style.boxShadow = "rgba(100, 50, 0, 0.56) 0px 22px 70px 4px";
+            }
+            if(document.getElementById("selected") != null){
+                document.getElementById("selected").removeAttribute('style');
+                document.getElementById("selected").style.animation = "shrinkT 1s"; //previousSide == "t" ? "shrinkT "+ "1s" : "shrinkCT " + "1s";
+                document.getElementById("selected").style.height = "2vw";
+                document.getElementById("selected").style.background = previousSide == "t" ? "rgba(255, 165, 0, 0.329)" : "rgba(0, 0, 255, 0.329)";
+                document.getElementById("selected").removeAttribute('id');
+            }
+            if(list[i].info != null){
+                infoBox.innerHTML = list[i].info;
+            }
+    
+            videoWindow.setAttribute('src', list[i].videoURL);
+            friendly.setAttribute('src', list[i].friendly);
+            crosshairView.setAttribute('src', list[i].crosshair);
+            crosshairDotView.setAttribute('src', list[i].crosshairDot);
+            enemy.setAttribute('src', list[i].enemy);
+            option.style.animation = list[i].side == "t" ? "growT "+ "1s" : "growCT " + "1s";
+            option.style.height = "4vw";
+            option.style.background = "rgba(255, 255, 255, 0.329);";
+            previousSide = list[i].side;
+            option.setAttribute('id', "selected");
+        }); 
+        document.getElementById("index").appendChild(option);
+    }
+}
+
+function fullScreenToggle() {
+    if(isFull == true){
+        videoWindow.style.width = "48vw";
+        videoWindow.style.left = "16vw";
+        videoWindow.style.top = "1vw";
+        document.getElementById("full").style.backgroundImage = "URL(icons/full.png)";
+        document.getElementById("full").style.left = "48vw";
+        document.getElementById("home").style.left = "44vw";
+        isFull = false;
+    }else{
+        videoWindow.style.width = "80vw";
+        videoWindow.style.left = "0vw";
+        videoWindow.style.top = "0vw";
+        document.getElementById("full").style.backgroundImage = "URL(icons/mini.png)";
+        document.getElementById("full").style.left = "76vw";
+        document.getElementById("home").style.left = "1vw";
+        isFull = true;
+    }
+}document.getElementById("full").addEventListener("click", fullScreenToggle);
+// document.getElementById("home").addEventListener("click", function () {
+//     if(isFull){fullScreenToggle();}
+//     document.getElementById("mediaContainer").style.visibility = "hidden";
+//     document.getElementById("index").style.animation = "";
+//     mapHolder.style.visibility = "visible";
+// });
